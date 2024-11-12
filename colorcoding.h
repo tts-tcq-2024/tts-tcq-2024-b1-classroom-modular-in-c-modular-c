a628d2863c97d3a7ff06cd1f9afebd019c5ff949
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2024-11-12 16:47:07 +0530

    Update colorcoding.h

--- colorcoding.h
+++ colorcoding.h
@@ -4,11 +4,6 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 #define MAX_COLORPAIR_NAME_CHARS 16
 
 typedef struct {
