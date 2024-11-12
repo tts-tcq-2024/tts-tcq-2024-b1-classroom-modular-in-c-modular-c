a5b215eb6520b808b988fecad902ecb319deafcf
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2024-11-12 16:52:33 +0530

    Update colorcoding.c

--- colorcoding.c
+++ colorcoding.c
@@ -1,10 +1,9 @@
 #include "colorcoding.h"
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
